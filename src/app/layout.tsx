30fc76d84f9f4f17bc83d44f92d996b9f1679fdc
unfor-dev <hamzayevasadulloh2005@gmail.com>
2025-04-24 14:39:26 +0500

    adding main resources

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,14 @@
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
+import "./app.css"
+import localFont from "next/font/local";
+
+const alpino = localFont({
+  src: "../../public/fonts/Alpino-Variable.woff2",
+  display: "swap",
+  weight: "100 900",
+  variable: "--font-alpino",
+});
 
 export default function RootLayout({
   children,
