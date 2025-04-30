6c9e6cf4d113ca78204d90f25050d0fa47eec3a1
unfor-dev <hamzayevasadulloh2005@gmail.com>
2025-04-30 23:18:01 +0500

    adding header title gsap animate

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { FC } from "react";
 import { asText, Content } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
@@ -5,6 +7,10 @@ import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { Bounded } from "@/components/Bounded";
 import Button from "@/components/Button";
 import { TextSplitter } from "@/components/TextSplitter";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
 
 /**
  * Props for `Hero`.
@@ -15,11 +21,25 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero: FC<HeroProps> = ({ slice }) => {
+
+  useGSAP(() => {
+    const introTl = gsap.timeline();
+
+    introTl.set('.hero', {opacity: 1})
+    .from(".hero-header-word", {
+      scale: 3,
+      opacity: 0,
+      ease: 'power4.in',
+      delay: 0.3,
+      stagger: 1,
+    })
+  })
+
   return (
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="hero"
+      className="hero opacity-0"
     >
       <div className="grid">
         <div className="grid h-screen place-items-center">
